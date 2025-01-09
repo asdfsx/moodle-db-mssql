f82a908ef7552c8e60aef7aa2270039d878291e0
asdfsx <asdfsx@gmail.com>
2025-01-09 13:40:06 +0800

    update waiting script

--- root/wait-for-mssql-to-come-up.sh
+++ root/wait-for-mssql-to-come-up.sh
@@ -6,10 +6,10 @@ i=0
 until /opt/mssql-tools18/bin/sqlcmd -No -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -d master -l 1 -Q "SELECT 1" > /dev/null 2>&1
 do
     echo "[moodle-db-mssql] Waiting for SQL to accept connections"
-    sleep 1
+    sleep 10
 
     i=$((i+1))
-    if [ $i -gt 60 ]; then
+    if [ $i -gt ${MSSQL_WAIT4READY:-60} ]; then
         echo "[moodle-db-mssql] timed out waiting for server to accept connections"
         exit 1;
     fi
